ddf9d03e0a2de9615a9022fb0af490623f339375
fengyeno <fengyeno@126.com>
2015-12-26 11:22:46 +0800

    2015-12-26

--- Data/Paypal-Express-Checkout/process.php
+++ Data/Paypal-Express-Checkout/process.php
@@ -40,7 +40,27 @@ if($_POST) //Post Data received from product list page.
 				'&L_PAYMENTREQUEST_0_DESC0='.urlencode($ItemDesc).
 				'&L_PAYMENTREQUEST_0_AMT0='.urlencode($ItemPrice).
 				'&L_PAYMENTREQUEST_0_QTY0='. urlencode($ItemQty).
-
+				
+				/* 
+				//Additional products (L_PAYMENTREQUEST_0_NAME0 becomes L_PAYMENTREQUEST_0_NAME1 and so on)
+				'&L_PAYMENTREQUEST_0_NAME1='.urlencode($ItemName2).
+				'&L_PAYMENTREQUEST_0_NUMBER1='.urlencode($ItemNumber2).
+				'&L_PAYMENTREQUEST_0_DESC1='.urlencode($ItemDesc2).
+				'&L_PAYMENTREQUEST_0_AMT1='.urlencode($ItemPrice2).
+				'&L_PAYMENTREQUEST_0_QTY1='. urlencode($ItemQty2).
+				*/
+				
+				/* 
+				//Override the buyer's shipping address stored on PayPal, The buyer cannot edit the overridden address.
+				'&ADDROVERRIDE=1'.
+				'&PAYMENTREQUEST_0_SHIPTONAME=J Smith'.
+				'&PAYMENTREQUEST_0_SHIPTOSTREET=1 Main St'.
+				'&PAYMENTREQUEST_0_SHIPTOCITY=San Jose'.
+				'&PAYMENTREQUEST_0_SHIPTOSTATE=CA'.
+				'&PAYMENTREQUEST_0_SHIPTOCOUNTRYCODE=US'.
+				'&PAYMENTREQUEST_0_SHIPTOZIP=95131'.
+				'&PAYMENTREQUEST_0_SHIPTOPHONENUM=408-967-4444'.
+				*/
 				
 				'&NOSHIPPING=0'. //set 1 to hide buyer's shipping address, in-case products that does not require shipping
 				
@@ -75,7 +95,7 @@ if($_POST) //Post Data received from product list page.
 		//We need to execute the "SetExpressCheckOut" method to obtain paypal token
 		$paypal= new MyPayPal();
 		$httpParsedResponseAr = $paypal->PPHttpPost('SetExpressCheckout', $padata, $PayPalApiUsername, $PayPalApiPassword, $PayPalApiSignature, $PayPalMode);
-
+		
 		//Respond according to message we receive from Paypal
 		if("SUCCESS" == strtoupper($httpParsedResponseAr["ACK"]) || "SUCCESSWITHWARNING" == strtoupper($httpParsedResponseAr["ACK"]))
 		{
